19fbe7b1732ad8023af6188a6fe7062c0b012bd5
jngoh-maker <j.ngoh@alustudent.com>
2026-06-25 10:14:48 -0700

    hello world

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-word = "Holberton"
-print(word[3:] + word[:2] + word[5:] + " Python")
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:67] + str[:6])
