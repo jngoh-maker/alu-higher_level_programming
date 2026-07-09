068b72185b69cdb03296583054494e81496a6c16
jngoh-maker <j.ngoh@alustudent.com>
2026-07-09 06:58:56 -0700

    Retrieve an element from a list

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """Return the element at the given index."""
+    if idx < 0 or idx >= len(my_list):
+        return None
+
+    return my_list[idx]
