627a95ddc26058962dd4391e1782e7239d832c41
jngoh-maker <j.ngoh@alustudent.com>
2026-07-10 12:23:08 -0700

    Add function to delete dictionary key

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
-        del a_dictionary[key]
-    return a_dictionary
+def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary):
+        print("{}: {}".format(key, a_dictionary[key]))
