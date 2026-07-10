5739dbb6f1bb15f46a774a67d0fb1da3ed937547
jngoh-maker <j.ngoh@alustudent.com>
2026-07-10 12:31:49 -0700

    Add function to compute square values of matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-
-    for row in matrix:
-        new_row = []
-        for number in row:
-            new_row.append(number ** 2)
-        new_matrix.append(new_row)
-
-    return new_matrix
+    return [[x**2 for x in row] for row in matrix]
